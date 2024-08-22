807bb2223bfdb9461bb1c3d86969f78dca46ad51
Guillaume Lugol <guillaume.lugol@gmail.com>
2024-08-22 12:54:54 +0200

    v2

--- dtect/Model/model_1.py
+++ dtect/Model/model_1.py
@@ -1,10 +1,8 @@
-import tensorflow as tf
-
 from tensorflow import keras
 from keras import Sequential, layers, models
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Conv2DTranspose, Input
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D, Input
 from keras.models import Model
-
+import numpy as np
 
 input_shape = (544,544,3)
 
@@ -12,32 +10,43 @@ def build_encoder(input_shape):
 
     encoder = Sequential()
 
-    encoder.add(layers.Conv2D(32, (3,3) , strides=(2,2), input_shape=input_shape, padding='same', activation='relu'))
+    encoder.add(layers.Conv2D(32, (3,3), input_shape=input_shape, padding='same', activation='relu'))
     encoder.add(layers.MaxPooling2D(2,2))
 
-    encoder.add(layers.Conv2D(64, (3,3),  strides=(2,2), padding='same', activation='relu'))
+    encoder.add(layers.Conv2D(64, (3,3),padding='same', activation='relu'))
     encoder.add(layers.MaxPooling2D(2,2))
 
-    encoder.add(layers.Conv2D(128, (3,3),  strides=(2,2), padding='same', activation='relu'))
+    encoder.add(layers.Conv2D(128, (3,3), padding='same', activation='relu'))
     encoder.add(layers.MaxPooling2D(2,2))
 
-    encoder.add(layers.Conv2D(256, (3,3),  strides=(2,2), padding='same', activation='relu'))
+    encoder.add(layers.Conv2D(256, (3,3), padding='same', activation='relu'))
+    encoder.add(layers.MaxPooling2D(2,2))
+
+    encoder.add(layers.Conv2D(512, (3, 3), padding='same', activation='relu'))
+    encoder.add(layers.MaxPooling2D(2, 2))
 
     return encoder
-
 
 
 def build_decoder():
 
     decoder = Sequential()
+    decoder.add(Conv2DTranspose(512, (3, 3), padding='same', activation='relu', input_shape=(17, 17, 512)))
+    decoder.add(UpSampling2D((2, 2)))
 
-    decoder.add(Conv2DTranspose(128, (3,3), strides=(2,2), padding='same', activation='relu'))
+    decoder.add(Conv2DTranspose(256, (3, 3), padding='same', activation='relu'))
+    decoder.add(UpSampling2D((2, 2)))
 
-    decoder.add(Conv2DTranspose(64, (3,3), strides=(2,2), padding='same', activation='relu'))
+    decoder.add(Conv2DTranspose(128, (3, 3), padding='same', activation='relu'))
+    decoder.add(UpSampling2D((2, 2)))
 
-    decoder.add(Conv2DTranspose(32, (3,3), strides=(2,2), padding='same', activation='relu'))
+    decoder.add(Conv2DTranspose(64, (3, 3), padding='same', activation='relu'))
+    decoder.add(UpSampling2D((2, 2)))
 
-    decoder.add(Conv2DTranspose(3, (3,3), strides=(2,2), padding='same', activation='sigmoid'))
+    decoder.add(Conv2DTranspose(32, (3, 3), padding='same', activation='relu'))
+    decoder.add(UpSampling2D((2, 2)))
+
+    decoder.add(Conv2DTranspose(3, (3, 3), padding='same', activation='sigmoid'))
 
     return decoder
 
@@ -51,18 +60,25 @@ def build_autoencoder(encoder, decoder):
 
 
 def compile_autoencoder(autoencoder):
-    autoencoder.compile(loss='binary_crossentropy',
+    autoencoder.compile(loss='mean_squared_error',
                   optimizer='adam',
                     metrics=['accuracy'])
-    return autoencoder
+
+
+def train_autoencoder(X_train, y_train, input_shape):
+    encoder = build_encoder(input_shape)
+    decoder = build_decoder()
+    autoencoder = build_autoencoder(encoder, decoder, input_shape)
+
+    compile_autoencoder(autoencoder)
+    history = autoencoder.fit(X_train, y_train, epochs=30, batch_size=15)
+    return history
 
 
 def main():
-    encoder = build_encoder(input_shape)
-    decoder = build_decoder()
-    autoencoder = build_autoencoder(encoder, decoder)
-    compile_autoencoder(autoencoder)
-
+    X_train = np.random.rand(25, 544, 544, 3)
+    y_train = np.random.rand(25, 544, 544, 3)
+    input_shape = (544, 544, 3)
 
 if __name__ == '__main__':
     main()
