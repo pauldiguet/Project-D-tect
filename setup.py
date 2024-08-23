186daf1c60e2dd8a337c52e07807a5ee60a9a7b6
Malo Guede <vincoss@proton.me>
2024-08-23 17:10:27 +0200

    update setup3

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ with open("Requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='dtect',
-      description="d-tect_wagon_project",
+      version="0.0.1",
+      description="",
+      license="MIT",
+      author_email="",
+      #url="https://github.com/lewagon/taxi-fare",
       install_requires=requirements,
-      packages=find_packages())
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
